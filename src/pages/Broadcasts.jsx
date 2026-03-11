db136889204a57795a9b8aa277565d8c11555926
hershel meisels <hershelmeisels@gmail.com>
2026-03-11 12:53:05 +0000

    File changes

--- src/pages/Broadcasts.jsx
+++ src/pages/Broadcasts.jsx
@@ -138,6 +138,7 @@ export default function Broadcasts() {
               onCancel={handleCancel}
               onDelete={setDeleteTarget}
               onViewReport={setReportBroadcast}
+              onEdit={setEditBroadcast}
             />
           ))}
         </div>
