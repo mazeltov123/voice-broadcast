b27703d19d4bbb335fffcb5efbcef2b377b8e225
hershel meisels <hershelmeisels@gmail.com>
2026-03-12 02:42:42 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -14,6 +14,7 @@ import {
   FileText,
   ShieldCheck,
   LogOut,
+  BarChart2,
 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { useCurrentUser } from "@/components/useCurrentUser";
